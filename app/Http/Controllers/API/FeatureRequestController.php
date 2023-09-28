ff3e4c2eca59eeb06285b2083df852683f7b47fd
unknown <aquib_j@yahoo.com>
2023-09-28 09:37:25 +0530

    Middleware Contructed added

--- app/Http/Controllers/API/FeatureRequestController.php
+++ app/Http/Controllers/API/FeatureRequestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\APIAuthentication;
 use Illuminate\Http\Request;
 use Auth;
 use App\Models\User;
@@ -15,6 +16,12 @@ use App\Http\Resources\FeatureRequestResource;
 
 class FeatureRequestController extends Controller
 {
+
+    // Apply the custom ApiKeyAuth middleware
+    
+    public function __construct(){
+        $this->middleware(APIAuthentication::class); 
+    }
 
 
     function index($id)  {
