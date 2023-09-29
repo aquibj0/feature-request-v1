f4fc260d6d7bdcff64a2abf0606f3e2a1f76eeeb
unknown <aquib_j@yahoo.com>
2023-09-29 09:59:07 +0530

    Vite Configuration updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,9 +44,9 @@
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ml-4">
+             <button style="background: indigo" class="bg-indigo-600 ml-3 inline-flex text-white items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs  uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 {{ __('Register') }}
-            </x-primary-button>
+            </button>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,8 +18,10 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    
                 </a>
+                <h3 class="fw-700" style="font-size: 2rem;font-weight:700"><a href="">
+                    FeaturesHub</a></h3>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
