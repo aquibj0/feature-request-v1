f493ca450bff3c836e26e26dee7d8a1e6af743a0
unknown <aquib_j@yahoo.com>
2023-10-01 12:49:15 +0530

    Close Button Added in the Status Compenent

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,10 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-md text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'font-medium text-md text-green-600 status-flash']) }}>
         {{ $status }}
+        <span class="text-end">
+            <button type="button" class="btn-close text-end" style="font-size: .7rem" onclick="$(this).parent().hide()" aria-label="Close"></button>
+        </span>
     </div>
 @endif
